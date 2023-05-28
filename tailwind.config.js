c6157469388a749fbea567de94e5778db1a338a4
fahmyfauzi <fahmyfauzii@gmail.com>
2023-05-28 17:05:29 +0700

    about section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-        container: {
-            center: true,
-            padding: '16px',
-        },
-        extend: {
-            colors: {
-                primary: '#14b8a6',
-                dark: '#0f172a'
-            },
-            screens: {
-                '2xl': '1320px'
-            },
-        },
-
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
     },
-    plugins: [],
-}
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
